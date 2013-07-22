c832692426a22860bfc3ded8c6d0e6b294a273bf
Jeff Sacco <jeffsacco@gmail.com>
2013-07-22 14:02:50 -0400

    Made the youtube popup a little more pretty

--- sites/all/themes/tundra/js/homer.js
+++ sites/all/themes/tundra/js/homer.js
@@ -1,6 +1,6 @@
 jQuery(function ($) {
   $('div#views_slideshow_cycle_main_front_page_slideshow-block div.views-row-0 div.slideshow-link a').click(function(){
-  	$.colorbox({html:"<iframe width=\"560\" height=\"315\" src=\"//www.youtube.com/embed/vwKYgjdT7fY\" frameborder=\"0\" allowfullscreen></iframe>"});
+  	$.colorbox({html:"<center><h2><img src=\"/sites/default/files/tundra_trip_small.png\"></h2></center><iframe width=\"560\" height=\"315\" src=\"//www.youtube.com/embed/vwKYgjdT7fY\" frameborder=\"0\" allowfullscreen></iframe>"});
   	return false;
   });
 });
