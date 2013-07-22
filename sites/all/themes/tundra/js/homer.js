f678120421f88efa56d3b3fef6609a66058ad1fc
Jeff Sacco <jeffsacco@gmail.com>
2013-07-22 14:14:04 -0400

    Bug in the youtube code.  Fixed

--- sites/all/themes/tundra/js/homer.js
+++ sites/all/themes/tundra/js/homer.js
@@ -1,5 +1,5 @@
 jQuery(function ($) {
-  $('div#views_slideshow_cycle_main_front_page_slideshow-block div.views-row-0 div.slideshow-link a').click(function(){
+  $('div#views_slideshow_cycle_main_front_page_slideshow-block div.views_slideshow_cycle_div_front_page_slideshow-block_0 div.views-row-0 div.slideshow-link a').click(function(){
   	$.colorbox({html:"<center><h2><img src=\"/sites/default/files/tundra_trip_small.png\"></h2></center><iframe width=\"560\" height=\"315\" src=\"//www.youtube.com/embed/vwKYgjdT7fY\" frameborder=\"0\" allowfullscreen></iframe>"});
   	return false;
   });
